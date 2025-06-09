f5435e059cfac51b4fbf6e2521ee3095af8a0783
JoyEmma12 <ejoy0600@gmail.com>
2025-06-09 11:49:09 +0100

    dashboard styling completed and responsive

--- my_final_year_project/src/components/Dashboard/QuickActions.jsx
+++ my_final_year_project/src/components/Dashboard/QuickActions.jsx
@@ -4,21 +4,19 @@ import { IoMdArrowForward } from "react-icons/io";
 
 const QuickActions = () => {
   return (
-    <div className="Quickaction-container">
-      <h3>Quick Actions</h3>
+    <div className="quick-actions-container">
+      <h3 className="quick-actions-title">Quick Actions</h3>
 
-      <div className="action-container d-flex flex-row flex-lg-row flex-sm-column gap-5">
-        <div className="lang-container">
+      <div className="quick-actions-cards">
+        <div className="quick-actions-card">
           <h4>Pick up where you left off and keep learning</h4>
-          <p></p>
-          <button className="startLearning-btn">
+          <button className="quick-actions-btn">
             Resume Learning <IoMdArrowForward />
           </button>
         </div>
-        <div className="lang-container">
+        <div className="quick-actions-card">
           <h4>Challenge yourself with a quick quiz</h4>
-          <p></p>
-          <button className="startLearning-btn">
+          <button className="quick-actions-btn">
             Start Quiz <IoMdArrowForward />
           </button>
         </div>
