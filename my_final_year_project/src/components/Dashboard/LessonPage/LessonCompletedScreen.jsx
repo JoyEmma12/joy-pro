b7653a249fc278e6b9a195febb5843c590c8bf1a
JoyEmma12 <ejoy0600@gmail.com>
2025-06-11 09:53:01 +0100

    ui updated and responsive

--- my_final_year_project/src/components/Dashboard/LessonPage/LessonCompletedScreen.jsx
+++ my_final_year_project/src/components/Dashboard/LessonPage/LessonCompletedScreen.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
-// import "./LessonCompleteScreen.css";
+import Confetti from "react-confetti";
+import useWindowSize from "react-use/lib/useWindowSize";
+import "./LessonCompletedScreen.css";
 
 const LessonCompleteScreen = () => {
   const { language, lessonId } = useParams();
   const navigate = useNavigate();
+  const { width, height } = useWindowSize();
+
 
   return (
     <div className="lesson-complete">
+      <Confetti width={width} height={height} />
       <h2>🎉 Lesson {lessonId} Completed!</h2>
       <p>
         You've successfully completed Lesson {lessonId} in {language}.
       </p>
-      <button onClick={() => navigate("/dashboard")}>Go to Dashboard</button>
+
+      <div className="button-group">
+        <button onClick={() => navigate("/dashboard")}>Go to Dashboard</button>
+
+      </div>
     </div>
   );
 };
