671b9e5bac1a658ef10dc2b7549fb47524bef9d9
JoyEmma12 <ejoy0600@gmail.com>
2025-07-26 23:41:21 +0100

    Add dynamic username and logout to Dashboard
    Dashboard now displays the logged-in user's name dynamically by reading from localStorage, and includes a logout button that clears localStorage and redirects to the login page. Updated styles for the navigation bar and logout button, and improved responsive layout.

--- my_final_year_project/src/components/Dashboard/Dashboard.jsx
+++ my_final_year_project/src/components/Dashboard/Dashboard.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { FaSignOutAlt } from "react-icons/fa";
 import Sidebar from "./Sidebar";
 import "./Dashboard.css";
 import DashboardProgress from "../Dashboard/LessonPage/DashboardProgress";
@@ -8,14 +10,39 @@ import QuickActions from "./QuickActions";
 
 const Dashboard = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const [username, setUsername] = useState("");
+
+  const navigate = useNavigate();
 
   const toggleSidebar = () => setCollapsed(!collapsed);
+
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate("/login");
+  };
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user && user.username) {
+      setUsername(user.username);
+    } else {
+      navigate("/");
+    }
+  }, []);
 
   return (
     <div className="dashboard-container">
       <Sidebar collapsed={collapsed} toggleSidebar={toggleSidebar} />
       <div className="dashboard-main">
-        <h2 className="greetings">Welcome back, Clinton!</h2>
+        <div className="dashboard-nav row align-items-center justify-content-between ">
+          <h2 className="greetings col-6">Welcome back, {username}</h2>
+
+          <button className="logout-btn col-2" onClick={handleLogout}>
+            <FaSignOutAlt />
+            Logout
+          </button>
+        </div>
+
         <div className="content-box">
           <DashboardProgress />
           <ResumeLearning />
